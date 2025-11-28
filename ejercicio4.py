e9d45857019ef5153b4b81a02f5bd88acd501bc2
RafaelLaria <rafael.lariaortiz@usp.ceu.es>
2025-11-28 11:50:53 +0100

    cambio

---
+++ ejercicio4.py
@@ -0,0 +1,13 @@
+class Lista:
+    def __init__(self):
+        self.lista = []
+    
+    def recorrer(self, lista):
+        for i in lista:
+            print(i)
+class Suma(Lista):
+    def __init__(self):
+        super().__init__(self)
+    
+    def sumar(self, lista):
+        for i in lista
