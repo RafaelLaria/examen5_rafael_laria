1638503b261829c6c698f93ea4a92737ddc6b6bc
RafaelLaria <rafael.lariaortiz@usp.ceu.es>
2025-11-28 15:06:58 +0100

    cambio

--- ejercicio4.py
+++ ejercicio4.py
@@ -1,13 +1,16 @@
-class Lista:
-    def __init__(self):
-        self.lista = []
-    
-    def recorrer(self, lista):
-        for i in lista:
-            print(i)
-class Suma(Lista):
-    def __init__(self):
-        super().__init__(self)
-    
+class Suma:
+    def __init__(self, lista):
+        self.lista = lista
+        self.suma = 0
+        self.resultado = self.sumar(lista)
+
     def sumar(self, lista):
-        for i in lista
+        for i in lista:
+           self.suma += i
+        return self.suma
+
+
+lista = [1,2,3,4,5,6,7,8,9]
+print(Suma(lista))
+resultado = Suma(lista)
+print(resultado.resultado)
